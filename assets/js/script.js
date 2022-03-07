62d4c6c9945cc7b4f668adf04d7249fc2ba95a82
Vinny D <vdurham@prideseals.com>
2022-03-07 12:59:59 -0500

    website is functionable, need to add local Save data and build css to improve the look

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ var temperatureEl = document.querySelector("#current-temp")
 var humidityEl = document.querySelector("#current-humidity")
 var windEl = document.querySelector("#wind-speed")
 var indexEl = document.querySelector("#uv-index")
+var cardEl = document.querySelector(".card-container")
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="
 
 function findWeather() {
@@ -63,17 +64,40 @@ var getIndex = function(uv) {
 
 var future = function(futureWeather) {
     for (var i = 1; i < 6; i++) {
-        var date = futureWeather.daily[i].dt;
-        console.log(date)
+        var card = document.createElement("div")
+        
+        var dateEl = document.createElement("h4");
+        var milliseconds = futureWeather.daily[i].dt * 1000;
+        var dateObject = new Date(milliseconds);
+        var getDate = dateObject.toLocaleString("en-US", {month: "numeric", day: "numeric", year: "numeric"})
+        dateEl.append(getDate)
 
-        var forecastImg = futureWeather.daily[i].weather[0].icon
-        console.log(forecastImg)
-        var forecastTemp = Math.round(((futureWeather.daily[i].temp.max - 273.15)*1.8) + 32);
-        console.log(forecastTemp)
-        var forecastWind = futureWeather.daily[i].wind_speed
-        console.log(forecastWind)
-        var forecastHumidity = futureWeather.daily[i].humidity
-        console.log(forecastHumidity)
+        var forecastImg = document.createElement("img");
+        forecastImg.setAttribute("src", "https://openweathermap.org/img/w/" + futureWeather.daily[i].weather[0].icon + ".png");
+        
+        var forecastTempEl = document.createElement("p");
+        var forecastTemp = ("Temp: " + Math.round(((futureWeather.daily[i].temp.max - 273.15)*1.8) + 32) + "°F");
+        forecastTempEl.append(forecastTemp);
+
+        var forecastWindEl = document.createElement("p");
+        var forecastWind = ("Wind: " + futureWeather.daily[i].wind_speed + "mph");
+        forecastWindEl.append(forecastWind);
+
+        var forecastHumidityEl = document.createElement("p");
+        var forecastHumidity = ("Humidity: " + futureWeather.daily[i].humidity + "%");
+        forecastHumidityEl.append(forecastHumidity);
+
+        card.append(dateEl);
+        card.append(forecastImg);
+        card.append(forecastTempEl);
+        card.append(forecastWindEl);
+        card.append(forecastHumidityEl);
+
+        card.classList.add("cards")
+        card.classList.add("col-md-3")
+
+        cardEl.append(card);
+        
     }
 
     
